908ceb3534950a6264525f580e4ded122026a2f3
Simone Occhipinti <simone.occhipinti00@gmail.com>
2024-05-21 13:44:32 +0200

    adjoint

--- case1.py
+++ case1.py
@@ -1,12 +1,12 @@
 # transport code
 
 import numpy as np
-import scipy as sp
-import scipy.optimize as opt
+import scipy.integrate as spint
 import numpy.random as rnd
 import matplotlib.pyplot as plt
 import materiale as mt
 import trasporto as tr
+import placzek
 
 #input data
 Cnat_total = np.loadtxt('cross_sections_Janis\Cnat_total.csv', delimiter=';',skiprows=3)
@@ -15,24 +15,27 @@ Cnat_scattering = np.loadtxt('cross_sections_Janis\Cnat_scattering.csv', delimit
 Onat_total = np.loadtxt('cross_sections_Janis\Onat_total.csv', delimiter=';',skiprows=3)
 Onat_scattering = np.loadtxt('cross_sections_Janis\Onat_scattering.csv', delimiter=';', skiprows=3)
 
-oxigen = mt.isotope(8,16,4*1.4E-3,50*Onat_total[:,0],Onat_total[:,1],Onat_scattering[:,1])
-carbon = mt.isotope(6,12,2.267,50*Cnat_total[:,0],Cnat_total[:,1],Cnat_scattering[:,1])
+#oxigen = mt.isotope(8,16,4*1.4E-3,Onat_total[:,0],Onat_total[:,1],Onat_scattering[:,1])
+#carbon = mt.isotope(6,12,2.267,Cnat_total[:,0],Cnat_total[:,1],Cnat_scattering[:,1])
 
 #oxigen = mt.isotope(8,16,4*1.4E-3,Onat_scattering[:,0],Onat_scattering[:,1],Onat_scattering[:,1])
-#carbon = mt.isotope(6,50,2.267,Cnat_scattering[:,0],Cnat_scattering[:,1],Cnat_scattering[:,1])
+carbon = mt.isotope(6,12,2.267,Cnat_scattering[:,0],Cnat_scattering[:,1],Cnat_scattering[:,1])
 
-composition = [oxigen,carbon]
-#composition = [carbon]
+#composition = [oxigen,carbon]
+composition = [carbon]
 medium = mt.material(composition)
 
-csi = tr.estimator(medium, 2E7, 1000)
-itermax = 1000
-
+csi = tr.estimator(medium)
+itermax = 20000
+#E0 = 20E6
+E0 = 2E6
 wd = 5
 wgt_min = 1/wd
 wgt_max = wd
 
 jj = 0
+
+#MC code
 while csi.iter<itermax:
     # inizio una nuova storia
     csi.new_iteration()
@@ -45,20 +48,22 @@ while csi.iter<itermax:
         jj+=1
 
     # genero una nuova particella dallo spettro di fissione (Watt)
-    e0 = tr.sample_watt(0.988,2.249)
-    #e0 = 2E6
+    #e0 = tr.sample_watt(0.988,2.249)
+    e0 = 2E6
     phi0 = 2*np.pi*rnd.random()
     mu0 = 2*rnd.random()-1
     teta0 = np.arccos(mu0)
     p0 = tr.Position(0.,0.,0.,'cartesian')
     d0 = tr.Direction(teta0,phi0)
 
-    nn = tr.particle(p0,d0,e0,1)
+    nn = tr.particle(p0,d0,e0,1,'adjunction')
     tt = 0
     # simulo il trasporto
     while eof > 0:
+
         # distanza per l'interazione
         flight = nn.sample_freeflight(medium)
+
         # conto l'interazione
         csi.new_interaction(medium,nn)
         
@@ -67,7 +72,7 @@ while csi.iter<itermax:
         nn.sample_energy(medium,composition)
         
         # aggiorno il peso dopo l'interazione
-        nn.sample_interaction(medium)
+        nn.sample_interaction(medium, composition)
 
         # check for russian roulette
         if nn.weight<=wgt_min:
@@ -78,23 +83,59 @@ while csi.iter<itermax:
                 eof = 0
         # energy check
         if nn.energy<=medium.energy[0] or nn.energy>=medium.energy[-1]:
-            #rho = rnd.rand()
-            #if rho<=1/wd:
-            #    nn.energy *= wd
-            #else:
             eof = 0
-        #if eof == 0:
-        #    plt.plot(csi.ref[0:maxenergy], csi.count[csi.iter-1][0:maxenergy])
-        #    plt.show()
 
 avg = csi.sample_avg()
 
-#fit_solution = np.poly1d(np.polyfit(csi.ref, avg, 8))
-#lethargy = -np.log(np.array(csi.ref)/2E6)
-#plt.plot(lethargy, avg)
-plt.plot(csi.ref, avg)
-#plt.xscale('log')
+lethargy = np.log(E0/csi.range)
+plotLet = np.array([(lethargy[ii]+lethargy[ii+1])/2 for ii in range(len(lethargy)-1)])
+deltaLet = np.diff(lethargy)
+eflx = avg/(-deltaLet)
+
+var = np.zeros(len(eflx))
+for ii in range(len(var)):
+    for jj in range(csi.iter):
+        var[ii] += ((csi.count[jj][ii]/deltaLet[ii] - eflx[ii])**2)/(csi.iter-1)
+sigma = (np.sqrt(var/csi.iter))
+
+#plc = placzek.placzek(12)
+plc = placzek.adj_placzek(12)
+#for ii in range(len(plc[1])):
+    #plc[1][ii] = plc[1][ii]/medium.xs_scattering(E0*np.exp(-plc[0][ii]))
+#    plc[1][ii] = plc[1][ii]/medium.xs_scattering(plc[0][ii])
+
+eps1 = E0*((carbon.alpha-1)/(carbon.alpha+1))**2
+eps2 = E0*((carbon.alpha-1)/(carbon.alpha+1))**4
+eps4 = E0*((carbon.alpha-1)/(carbon.alpha+1))**16
+
+#plt.plot(plotLet, eflx,  linewidth=4, label='MC results')
+#plt.plot(plotLet,eflx-sigma,'o',markersize=1, c='black')
+#plt.plot(plotLet,eflx+sigma,'o',markersize=1, c='black')
+
+plt.plot(csi.ref, eflx,  linewidth=4, label='MC results')
+plt.plot(csi.ref,eflx-sigma,'o',markersize=1, c='black')
+plt.plot(csi.ref,eflx+sigma,'o',markersize=1, c='black')
+
+plt.plot(plc[0],plc[1],linewidth=1.5,label='Placzek function')
+
+#plt.axvline(x=eps1, color='r', linestyle='--', linewidth=0.5,label=r'$\alpha$ E0')
+#plt.axvline(x=eps2, color='r', linestyle='--', linewidth=0.5,label=r'$\alpha^2$E0')
+
+#plt.axvline(x=np.log(1/((carbon.alpha-1)/(carbon.alpha+1))**2), color='r', linestyle='--', linewidth=0.5,label=r'$\alpha$ E0')
+#plt.axvline(x=np.log(1/((carbon.alpha-1)/(carbon.alpha+1))**4), color='r', linestyle='--', linewidth=0.5,label=r'$\alpha^2$E0')
+
+#plt.title('Direct transport problem')
+plt.title('Adjoint transport problem')
+
+
+#plt.xlabel('lethargy [-]')
+plt.xlabel('energy [eV]')
+#plt.ylabel(r'$\Phi$ [n/cm2/s/eV]')
+plt.ylabel(r'$\Sigma_s\Psi$ [-/cm3/s]')
+#plt.ylabel(r'$\Psi [-/cm2/s]$')
+plt.xlim(2E6,20E6)
+plt.xlim(E0)
+plt.legend()
+plt.xscale('log')
 #plt.yscale('log')
 plt.show()
-
-
--- trasporto.py
+++ trasporto.py
@@ -2,8 +2,7 @@
 
 import numpy as np
 import numpy.random as rnd
-import scipy as sp
-import scipy.optimize as opt
+import scipy.integrate as spi
 import materiale
 
 class Cartesian:
@@ -11,15 +10,16 @@ class Cartesian:
         self.x=ics
         self.y=ips 
         self.z=zeta
+    @property    
     def distance(self):
         return np.sqrt(self.x**2+self.y**2+self.z**2)
     def cartesian2polar(self):
-        if self.distance() == 0:
+        if self.distance == 0:
             rr = 0
             teta = 0
             phi = 0
         else:
-            rr = self.distance()
+            rr = self.distance
             teta = np.arccos(self.z/rr)
             phi = np.arcsin(self.y/(rr*np.sin(teta)))
         return Polar(rr,teta,phi)
@@ -50,11 +50,13 @@ class Direction:
         self.mu = np.cos(self.teta)
 
 class particle:
-    def __init__(self, point=Position, angle=Direction, en=float, wgt=float):
+    def __init__(self, point=Position, angle=Direction, en=float, wgt=float, tp = str):
         self.position = point
         self.direction = angle
         self.energy = en
         self.weight = wgt
+        self.type = tp
+
     def newposition(self, ll=float):
         self.position.cartesian.z += ll*np.cos(self.direction.teta)
         self.position.cartesian.x += ll*np.sin(self.direction.teta)*np.cos(self.direction.phi)
@@ -82,9 +84,20 @@ class particle:
         ll = -1/mat.xs_total(self.energy)*np.log(rho)
         return ll
     
-    def sample_interaction(self, mat=materiale.material):
+    def sample_interaction(self, mat=materiale.material,parts=list[materiale.material]):
         #implicit capture, no fission
         self.weight *= mat.xs_scattering(self.energy)/mat.xs_total(self.energy)
+
+        if self.type == 'adjunction':
+            ii = self.sample_isotope(mat,parts,self.energy)
+            alfa = ((parts[ii].alpha-1)/(parts[ii].alpha+1))**2
+            low_i = np.where(self.energy>=mat.energy)[0][-1]
+            if self.energy/alfa <= mat.energy[-1]:
+                up_i = np.where(self.energy/alfa<=mat.energy)[0][0]
+            else:
+                up_i = -1
+            SS = np.trapz(mat.macro_scattering[low_i:up_i]/mat.energy[low_i:up_i], mat.energy[low_i:up_i])
+            self.weight *= 1/(1-alfa)/mat.xs_scattering(self.energy)*SS
 
     def sample_direction(self):
         self.direction.mu = rnd.rand()*2-1
@@ -95,33 +108,46 @@ class particle:
         # step function
         ii = self.sample_isotope(mat,parts,self.energy)
         alfa = ((parts[ii].alpha-1)/(parts[ii].alpha+1))**2
-        new_ee = alfa*self.energy+(1-alfa)*self.energy*rnd.rand()
-        self.energy = new_ee
+
+        if self.type == 'neutron':
+            new_ee = alfa*self.energy+(1-alfa)*self.energy*rnd.rand()
+            self.energy = new_ee
+
+        elif self.type == 'adjunction':
+            low_i = np.where(self.energy>=mat.energy)[0][-1]
+            if self.energy/alfa <= mat.energy[-1]:
+                up_i = np.where(self.energy/alfa<=mat.energy)[0][0]
+            else:
+                up_i = -1
+            SS = np.trapz(mat.macro_scattering[low_i:up_i]/mat.energy[low_i:up_i], mat.energy[low_i:up_i])
+            exponent = rnd.rand() * SS / mat.xs_scattering(self.energy)
+            # Using logarithm to avoid overflow
+            result_log = np.log(self.energy) + exponent
+            if result_log>= np.log(mat.energy[-1]):
+                self.energy = mat.energy[-1]+1
+            else:
+                self.energy = np.exp(result_log)
 
 class estimator:
-    def __init__(self, mm = materiale.material, max_en=float,nn=int):
+    def __init__(self, mm = materiale.material):
         self.count = []
-        #self.range = mm.energy
-        self.range = np.linspace(mm.energy[0], max_en, nn)        
-        self.ref = np.array([(self.range[ii]+self.range[ii+1])/2 for ii in range(len(self.range)-1)])
+        self.range = mm.energy
+        self.ref = np.array([(self.range[ii]+self.range[ii+1])/2 for ii in range(len(self.range)-1)])   
         self.iter = 0
     
     def new_interaction(self, mm=materiale.material, nn = particle):
         index = np.where(nn.energy<=self.range)[0][0]
-        self.count[self.iter-1][index-1] += nn.weight/mm.xs_total(self.ref[index-1])
-        #self.count[self.iter-1][index-1] += nn.weight
+        #self.count[self.iter-1][index-1] += nn.weight/mm.xs_scattering(self.ref[index-1])
+        self.count[self.iter-1][index-1] += nn.weight
 
     def new_iteration(self):
         self.count.append(np.zeros(len(self.ref)))
         self.iter += 1
 
     def sample_avg(self):
-        total = np.zeros(len(self.ref))
         avg = np.zeros(len(self.ref))
         for ii in range(self.iter):
-            total += self.count[ii]
-        for jj in range(len(self.ref)):
-            avg[jj] += total[jj]/self.iter/(self.range[jj+1]-self.range[jj])
+            avg += self.count[ii]/self.iter
         return avg
 
 def sample_watt(aa=float, bb=float):
